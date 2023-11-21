2b3dda1e38bac5debc6083b52d22c2982a218016
Penyo <penyoofficial@outlook.com>
2023-11-21 11:27:47 +0800

    feat: add an annotation that notices developers to rebuild

---
+++ src/main/java/com/penyo/herbms/util/NeedRebuild.java
@@ -0,0 +1,12 @@
+package com.penyo.herbms.util;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+/**
+ * 需要重构的警告
+ */
+@Retention(RetentionPolicy.SOURCE)
+@Deprecated
+public @interface NeedRebuild {
+}
